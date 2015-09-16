23a7f2030102224eec83ce5c4e297075e3cc140a
mitchashby16 <mitchashby16@gmail.com>
2015-09-15 22:50:28 -0600

    filtered through id json to make them integers for fun

--- app/models/concerns/finder.rb
+++ app/models/concerns/finder.rb
@@ -3,13 +3,13 @@ module Finder
 
   module ClassMethods
     def find_by_attribute(params)
-      ActiveRecord::Base.connection.execute(get_sql(params)).first
+      filter_ids(ActiveRecord::Base.connection.execute(get_sql(params)).first)
     end
 
     def find_all_matches(params)
       results = ActiveRecord::Base.connection.execute(get_sql(params))
       results.map do |result|
-        result
+        filter_ids(result)
       end
     end
 
@@ -20,12 +20,26 @@ module Finder
 
       sql = "SELECT * FROM #{params[:controller].split("/").last.capitalize}
       WHERE id='#{random}';"
-      ActiveRecord::Base.connection.execute(sql).first
+      filter_ids(ActiveRecord::Base.connection.execute(sql).first)
     end
 
     def get_sql(params)
       "SELECT * FROM #{params[:controller].split("/").last.capitalize}
       WHERE #{params.keys.first}='#{params.values.first}';"
+    end
+
+    def filter_ids(result)
+      hash_keys = result.keys.select do |key|
+        key[-2..-1] == 'id'
+      end
+      parse(result, hash_keys)
+    end
+
+    def parse(result, keys)
+      keys.each do |key|
+        result[key] = result[key].to_i if result[key]
+      end
+      result
     end
   end
 end
